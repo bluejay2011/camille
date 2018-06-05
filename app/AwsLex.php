aefdea510f9fab9019f6f3fa8ed2940a6312b24d
Steph Jose <stephjose2011@gmail.com>
2018-06-05 17:04:30 +0800

    update data

--- app/AwsLex.php
+++ app/AwsLex.php
@@ -12,8 +12,8 @@ class AwsLex extends Model
             'region'  => 'us-east-1',
             'version' => 'latest',
             'credentials' => [
-                'key'    => 'AKIAJVQLHMNINXIS5IWA',
-                'secret' => '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',
+                'key'    => '',
+                'secret' => '',
             ]
         ];
     }
